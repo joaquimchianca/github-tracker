4ec4dd58c81a71230c8ad7f31a5d381c4bd4c3aa
Joaquim Chianca <joaquimchianca@gmail.com>
2023-05-12 15:17:28 -0300

    created component Titulo

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
+    import Titulo from "./components/Titulo.svelte";
+
 </script>
 
-<main>
-  
-</main>
+<div class="app">
+    <header>
+        <Titulo/>
+    </header>
+</div>
 
 <style>
+  .app {
+    max-height: 100vh;
+  }
+
+  header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
   
 </style>
