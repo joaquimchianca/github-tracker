ab0bd33c1e3e65c3bc9cc636059b59de852422b5
Joaquim Chianca <joaquimchianca@gmail.com>
2023-05-15 12:37:56 -0300

    created Cabecalho component

---
+++ src/components/Cabecalho.svelte
@@ -0,0 +1,27 @@
+<!-- TS -->
+<script lang="ts">
+    import Formulario from "./Formulario.svelte";
+    import Titulo from "./Titulo.svelte";
+</script>
+
+<!-- HTML -->
+<header>
+    <Titulo />
+    <div class="busca__usuario">
+        <Formulario on:changeUser />
+    </div>
+</header>
+
+<!-- CSS -->
+<style>
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .busca__usuario {
+        position: relative;
+        width: 70%;
+    }
+</style>
