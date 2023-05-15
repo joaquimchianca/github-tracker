7b1771870336989d285489941f96b0820474a81a
Joaquim Chianca <joaquimchianca@gmail.com>
2023-05-14 23:16:20 -0300

    created interface for github profile

---
+++ src/interface/IUsuario.ts
@@ -0,0 +1,9 @@
+export default interface IUsuario {
+    login: string
+    avatar: string
+    profilelUrl: string
+    name: string
+    location: string
+    repositorios: number
+    followers: number
+}
