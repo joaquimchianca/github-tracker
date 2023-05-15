976e45ae89e5590789eab257ffc9f024697ae95b
Joaquim Chianca <joaquimchianca@gmail.com>
2023-05-14 23:15:31 -0300

    created BarraSuperior component

---
+++ src/components/BarraSuperior.svelte
@@ -0,0 +1,32 @@
+<div class="barra__superior">
+    <span class="acao"></span>
+    <span class="acao"></span>
+    <span class="acao"></span>
+</div>
+
+<style>
+    .barra__superior {
+        background: rgba(255,255,255,0.5);
+        padding: 10px;
+        border-radius: 8px 8px 0 0;
+    }
+
+    .barra__superior > .acao {
+        width: 7px;
+        height: 7px;
+        display: inline-block;
+        border-radius: 50%;
+    }
+
+    .barra__superior > .acao:first-child {
+        background: #d8695b
+    }
+
+    .barra__superior > .acao:nth-child(2) {
+        background: #eac213;
+    }
+
+    .barra__superior > .acao:nth-child(3) {
+        background: #79c743;
+    }
+</style>
